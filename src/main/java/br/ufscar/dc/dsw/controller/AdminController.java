81f4fb1a031e14cedef2fd79f5f6956767f202cd
João Gabriel Gonçalves <joaogg123@gmail.com>
2021-10-25 12:59:43 -0300

    Cria user funcional

--- src/main/java/br/ufscar/dc/dsw/controller/AdminController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/AdminController.java
@@ -1,11 +1,21 @@
 package br.ufscar.dc.dsw.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.ufscar.dc.dsw.domain.Usuario;
 import br.ufscar.dc.dsw.service.spec.IUsuarioService;
 
 @Controller
@@ -18,7 +28,36 @@ public class AdminController {
 	private BCryptPasswordEncoder encoder;
 
     @GetMapping("/adminindex")
-    public String home() {
+    public String adminindex() {
         return "admin/adminindex";
+    }
+
+    @GetMapping("/listausers")
+    public String lista(ModelMap model) {
+        List<Usuario> lista = service.buscarTodos();
+        List<Usuario> listafiltrada = new ArrayList<Usuario>();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getRole().contains("ROLE_USER")) {
+                listafiltrada.add(lista.get(i));
+            }
+        }
+        model.addAttribute("usuarios", listafiltrada);
+        return "admin/listausers";
+    }
+
+    @GetMapping("/cadastrouser")
+    public String cadastro(Usuario usuario) {
+        return "admin/cadastrouser";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) {
+            return "admin/cadastrouser";
+        }
+        usuario.setPassword(encoder.encode(usuario.getPassword()));
+		service.salvar(usuario);
+        attr.addFlashAttribute("sucess", "Usuário inserido com sucesso.");
+        return "redirect:/admin/listausers";
     }
 }
